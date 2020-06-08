a102beb3d2094835de5062a4377513c03054d8d8
Rodrigo Hernan Ramos <rodrigohernan.ramos@gmail.com>
2020-06-08 13:11:25 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 sudo apt-get remove vim-tiny
+#pre requisites
+sudo apt-get install libncurses5-dev libncursesw5-dev
 
 # Create the directories you need
 sudo mkdir -p /opt/local/bin
@@ -6,10 +8,10 @@ sudo mkdir -p /opt/local/bin
 cd ~
 git clone https://github.com/vim/vim.git && cd vim
 ./configure --prefix=/opt/local --enable-python3interp=yes
-make
+sudo make
 sudo make install
 
-# install plugin for install plugins 
+# install plugin for install plugins
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # copy vimrc
@@ -18,10 +20,10 @@ curl https://raw.githubusercontent.com/RodrigoHernan/vim/master/vimrc.vim --outp
 
 # install plugin autocompleteMe
 sudo apt install build-essential cmake python3-dev
-git clone https://github.com/Valloric/YouCompleteMe.git --output ~/.vim/bundle/
+git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
 cd ~/.vim/bundle/YouCompleteMe
 git submodule update --init --recursive
 python3 install.py --clang-completer
-python3 install.py --all
+python3 install.py --cs-completer --ts-completer
 
 vim +PluginInstall +qall
